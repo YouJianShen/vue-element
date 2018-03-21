fed59b47bb0888c7713e5b3363f3c67d700957bf
suitorS <771643834@qq.com>
2018-03-21 18:13:02 +0800

    initial

--- src/routes.js
+++ src/routes.js
@@ -30,10 +30,10 @@ let routes = [
         name: "导航一",
         iconCls: "el-icon-message", // 图标样式class
         children: [
-            { path: "/main", component: Main, name: "主页", hidden: true },
-            { path: "/table", component: Table, name: "Table" },
-            { path: "/form", component: Form, name: "Form" },
-            { path: "/user", component: user, name: "列表" }
+            {path: "/main", component: Main, name: "主页", hidden: true},
+            {path: "/table", component: Table, name: "Table"},
+            {path: "/form", component: Form, name: "Form"},
+            {path: "/user", component: user, name: "列表"}
         ]
     },
     {
@@ -42,8 +42,8 @@ let routes = [
         name: "导航二",
         iconCls: "fa fa-id-card-o",
         children: [
-            { path: "/page4", component: Page4, name: "页面4" },
-            { path: "/page5", component: Page5, name: "页面5" }
+            {path: "/page4", component: Page4, name: "页面4"},
+            {path: "/page5", component: Page5, name: "页面5"}
         ]
     },
     {
@@ -53,7 +53,7 @@ let routes = [
         iconCls: "fa fa-address-card",
         leaf: true, // 只有一个节点
         children: [
-            { path: "/page6", component: Page6, name: "导航三" }
+            {path: "/page6", component: Page6, name: "导航三"}
         ]
     },
     {
@@ -62,13 +62,13 @@ let routes = [
         name: "Charts",
         iconCls: "fa fa-bar-chart",
         children: [
-            { path: "/echarts", component: echarts, name: "echarts" }
+            {path: "/echarts", component: echarts, name: "echarts"}
         ]
     },
     {
         path: "*",
         hidden: true,
-        redirect: { path: "/404" }
+        redirect: {path: "/404"}
     }
 ];
 
