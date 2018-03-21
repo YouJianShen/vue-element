7ca6fe3b52b1864faf94f01ce83cb5ecb2ece94b
suitorS <771643834@qq.com>
2018-03-21 18:18:17 +0800

    initial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,16 +23,23 @@
 		<el-col :span="24" class="main">
 			<aside :class="collapsed?'menu-collapsed':'menu-expanded'">
 				<!--导航菜单-->
-				<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
-					 unique-opened router v-show="!collapsed">
-					<template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-						<el-submenu :index="index+''" v-if="!item.leaf">
-							<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-							<el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
-						</el-submenu>
-						<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+				<el-menu class="el-menu-vertical-demo" :default-active="$route.path" v-show="!collapsed">
+					<template v-for="item in router.options.routes">
+						<el-menu-item v-if="!item.hidden">{{item.name}}</el-menu-item>
 					</template>
 				</el-menu>
+
+
+				<!--<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"-->
+					 <!--unique-opened router v-show="!collapsed">-->
+					<!--<template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">-->
+						<!--<el-submenu :index="index+''" v-if="!item.leaf">-->
+							<!--<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>-->
+							<!--<el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>-->
+						<!--</el-submenu>-->
+						<!--<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>-->
+					<!--</template>-->
+				<!--</el-menu>-->
 				<!--导航菜单-折叠后-->
 				<ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
 					<li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
